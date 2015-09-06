67128c23abf0792bd60ba1ceacbbf1d8a038a061
panxinx <panxinx@aol.com>
2015-09-06 13:42:23 -0700

    init

--- com.xpan.java.algorithms/src/com/xpan/java/algorithms/DuplicateChar.java
+++ com.xpan.java.algorithms/src/com/xpan/java/algorithms/DuplicateChar.java
@@ -1,5 +1,23 @@
 package com.xpan.java.algorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DuplicateChar {
 
+	public boolean hasDuplicateCharacter(String contents) {
+		boolean find = false;
+		List<String> tList = new ArrayList<String>();
+		for (int idx = 0; idx < contents.length(); idx++) {
+			char ch = contents.charAt(idx);
+			if (tList.contains(ch)) {
+				find = true;
+				break;
+			} else {
+				tList.add(String.valueOf(ch));
+			}
+		}
+
+		return find;
+	}
 }
